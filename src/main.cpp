bed86839f3c19524325b020248f3cf8320abda3e
Metizik <mokych.apriorit@gmail.com>
2018-10-19 01:34:30 +0300

    Added UsbMuxD to cmake project

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "idevicecpp/idevicecpp.h"
 #include <plist/plist.h>
+#include <usbmuxd.h>
 
 #ifdef idevicecpp
 int main(int, char**)
 {
+    usbmuxd_connect(0,0);
     plist_new_bool(1);
    std::cout << "Hello, world!\n";
 }
