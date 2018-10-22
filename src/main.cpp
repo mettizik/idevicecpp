d263683b2b4ff1dc8bc70f01298b0cf9d3cdf321
Metizik <mokych.apriorit@gmail.com>
2018-10-23 00:17:59 +0300

    Started to work on plist wrapper

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,85 @@
 #include <iostream>
+#include <memory>
 #include "idevicecpp/idevicecpp.h"
 #include <plist/plist.h>
 #include <usbmuxd.h>
+#include <libimobiledevice/libimobiledevice.h>
+#include <libimobiledevice/lockdown.h>
 
-#ifdef idevicecpp
+class Node
+{
+public:
+    Node(plist_t node)
+        : m_node(node)
+    {
+        if (!m_node) {
+            m_node = plist_new_bool(0);
+        }
+    }
+
+    Node& operator= (const std::string& value)
+    {
+        plist_set_string_val(m_node, value.c_str());
+    }
+
+    operator plist_t() {
+        return m_node;
+    }
+
+private:
+    plist_t m_node;
+};
+
+class Plist
+{
+public:
+    explicit Plist(plist_t root)
+        : m_root(root)
+    {}
+
+    Node operator [](const std::string& item) const {
+        plist_t child = plist_dict_get_item(m_root, item.c_str());
+        if (child != nullptr) {
+            return Node(plist_copy(child));
+        }
+        throw std::runtime_error("Key is not set: " + item);
+    }
+
+    Node operator [](const std::string& item) {
+        Node node = (plist_dict_get_item(m_root, item.c_str()));
+        plist_dict_set_item(m_root, item.c_str(), node);
+    }
+
+    Node at(const std::string& key) {
+        auto nodeval = plist_dict_get_item(m_root, key.c_str());
+        if (nodeval) {
+            return Node(nodeval);
+        }
+        throw std::runtime_error("No such key: " + key);
+    }
+
+    plist_t operator*() {
+        return m_root;
+    }
+
+    ~Plist()
+    {
+        plist_free(m_root);
+    }
+
+private:
+    plist_t m_root;
+};
+
 int main(int, char**)
 {
-    usbmuxd_connect(0,0);
-    plist_new_bool(1);
-   std::cout << "Hello, world!\n";
+    try {
+        Plist pl(plist_new_dict());
+        pl["key"] = "value";
+        pl.at("key");
+        return 0;
+    } catch (const std::exception& ex) {
+        std::cerr << ex.what() << std::endl;
+        return 1;
+    }
 }
-#endif
