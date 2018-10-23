b35d3434e8eb7981fd4413fe51d9793ca033f1f4
Mokych Andrey <mokych.andrey@apriorit.com>
2018-10-23 15:22:08 +0300

    Added sh build script

---
+++ build_unix.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo This script is not tested on any UNIX system, but should be working
+mkdir -p build
+pushd build
+    conan install OpenSSL/1.1.0i@conan/stable -g cmake 
+    cmake -G "Unix Makefiles" ..
+    cmake --build . --target idevicecpp
+popd 
