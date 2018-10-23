96a66ab05bd25b1bbef90b3867a2da6a6a47d655
Mokych Andrey <mokych.andrey@apriorit.com>
2018-10-23 15:26:42 +0300

    Added cleaning of build directory

--- build_unix.sh
+++ build_unix.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo This script is not tested on any UNIX system, but should be working
+rm -rdf build
 mkdir -p build
 pushd build
     conan install OpenSSL/1.1.0i@conan/stable -g cmake 
